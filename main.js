ecb6484eeba3c2bdb4f4034616bda57dcbd26ed4
sahilparikh98 <sparikh98@gmail.com>
2020-11-02 18:44:44 -0600

    fix context menu showing on right and left click

--- main.js
+++ main.js
@@ -1,12 +1,21 @@
 const { menubar } = require('menubar');
+const { app, Menu, Tray } = require('electron');
 
-const mb = menubar({
-  index: "https://www.notion.so",
-  icon: "icon-white.png",
-  showDockIcon: false,
-  showOnAllWorkspaces: false,
+app.on('ready', () => {
+  'use-strict';
+  let tray = new Tray("icon-white.png");
+  const contextMenu = Menu.buildFromTemplate([{role: "quit"}]);
+
+  const mb = menubar({
+    index: "https://www.notion.so",
+    showDockIcon: false,
+    showOnAllWorkspaces: false,
+    tray: tray,
+  });
+
+  mb.on('ready', () => {
+    tray.on('right-click', () => {
+      mb.tray.popUpContextMenu(contextMenu);
+    });
+  });
 });
-
-mb.on('ready', () => {
-  console.log("menubar is ready");
-})
